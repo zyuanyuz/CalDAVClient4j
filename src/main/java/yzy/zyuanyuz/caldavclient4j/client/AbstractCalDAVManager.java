c47c521e14e4c7108b0763c9d2f8212178f511b8
zyuanyuz <zyuanyuz@outlook.com>
2019-10-15 22:19:20 +0800

    one step

---
+++ src/main/java/yzy/zyuanyuz/caldavclient4j/client/AbstractCalDAVManager.java
@@ -0,0 +1,10 @@
+package yzy.zyuanyuz.caldavclient4j.client;
+
+/**
+ * @author zyuanyuz
+ * @since 2019/10/15 20:59
+ */
+
+public class AbstractCalDAVManager {
+
+}
---
+++ src/main/java/yzy/zyuanyuz/caldavclient4j/client/icloud/ICloudCalDAVManager.java
@@ -0,0 +1,9 @@
+package yzy.zyuanyuz.caldavclient4j.client.icloud;
+
+/**
+ * @author zyuanyuz
+ * @since 2019/10/15 20:59
+ */
+
+public class ICloudCalDAVManager extends Abstract{
+}
