dee49fd5ddc44e28a410002f1fab824818034f71
george.yu <zyuanyuz@outlook.com>
2019-11-22 16:51:01 +0800

    remove CalDAVAppleManager

--- src/main/java/yzy/zyuanyuz/caldavclient4j/controller/AppleCalendarController.java
+++ src/main/java/yzy/zyuanyuz/caldavclient4j/controller/AppleCalendarController.java
@@ -1,10 +1,8 @@
 package yzy.zyuanyuz.caldavclient4j.controller;
 
-import net.fortuna.ical4j.model.Calendar;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import yzy.zyuanyuz.caldavclient4j.client.CalDavAppleManager;
 
 /**
  * @author George Yu
@@ -14,34 +12,4 @@ import yzy.zyuanyuz.caldavclient4j.client.CalDavAppleManager;
 @RequestMapping("/apple")
 public class AppleCalendarController {
 
-  @GetMapping("/cal")
-  public String getAppleCal() throws Exception {
-    CalDavAppleManager calDavAppleManager = new CalDavAppleManager();
-    return null;
-  }
-
-  @GetMapping("/getReport")
-  public String getReport() throws Exception {
-    CalDavAppleManager calDavAppleManager = new CalDavAppleManager();
-    return calDavAppleManager.getReportWithAuth().toString();
-  }
-
-  @GetMapping("/addEvent")
-  public void addEvent() throws Exception {
-    CalDavAppleManager calDavAppleManager = new CalDavAppleManager();
-    calDavAppleManager.addEvent();
-  }
-
-  @GetMapping("/getETag")
-  public String getETag() throws Exception {
-    CalDavAppleManager calDavappleManager = new CalDavAppleManager();
-    return calDavappleManager.getETag();
-  }
-
-  @GetMapping("/test")
-  public String getTest() throws Exception{
-    CalDavAppleManager calDavAppleManager = new CalDavAppleManager();
-    calDavAppleManager.testSub();
-    return "asd";
-  }
 }
