94ce0c6a634e169b47cf1b6e359b6da99cdacfbd
zyuanyuz <zyuanyuz@outlook.com>
2019-12-29 23:17:35 +0800

    one step

--- src/main/java/yzy/zyuanyuz/caldavclient4j/client/icloud/ICloudCalendar.java
+++ src/main/java/yzy/zyuanyuz/caldavclient4j/client/icloud/ICloudCalendar.java
@@ -6,35 +6,39 @@ import yzy.zyuanyuz.caldavclient4j.client.AbstractCalDAVCalendar;
  * @author zyuanyuz
  * @since 2019/12/28 22:09
  */
-
 public class ICloudCalendar extends AbstractCalDAVCalendar {
 
+  public IResource iResource() {
+    return new IResource();
+  }
 
+  public class IResource {}
 
-    public IResource iResource(){
-        return new IResource();
+  public IEvent iEvent() {
+    return new IEvent();
+  }
+
+  public class IEvent {
+    public Get get(String resourceId) {
+      return new Get();
     }
 
-    public class IResource{
+    public class Get {}
 
+    public List list(String resourceId) {
+      return new List();
     }
 
-    public IEvent iEvent(){
-        return new IEvent();
+    public class List {}
+  }
+
+  /** ICloudCalendar builder */
+  public static final class Builder {
+    public Builder() {}
+
+    public ICloudCalendar build() {
+      // new ICloudCalendar(this);
+      return null;
     }
-
-    public class IEvent {
-
-    }
-
-    public static final class Builder{
-        public Builder(){
-
-        }
-        public ICloudCalendar build(){
-            //new ICloudCalendar(this);
-            return null;
-        }
-    }
-
+  }
 }
